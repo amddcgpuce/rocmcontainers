06651a87993d7d066f595ecfc247040ac7387181
Roohollah Etemadi <roohollah.etemadi@amd.com>
2022-05-06 20:22:48 +0000

    Created README, Docker and Singularity run scripts for AMD Infinity Hub user manual for openmm

---
+++ openmm/run_openmm_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -x
+#************************************************************************************************
+# Usage: sudo sh run_openmm_docker.sh                                                                 *
+# Copyright (c) 2022 Advanced Micro Devices, Inc. All Rights Reserved.                          *
+# This script is used to run AMD Infiniy Openmm Docker Container                                *
+# Contact info: roohollah.etemadi@amd.com                                                       *
+# Version: V1.0                                                                                   * 
+# Modified: 2022-04-26                                                                          *
+# Version History:                                                                              *
+# V1.0: run amdih/openmm:7.7.0_49 docker commands                                                *
+#************************************************************************************************
+
+echo "Date:`date`"
+
+echo "==== Pull OpenMM docker image  ===="
+echo "docker pull amdih/openmm:7.7.0_49"
+docker pull amdih/openmm:7.7.0_49
+echo "==== Pull complete  ===="
+
+echo "==== Run OpenMM docker image in a noninteractive way  ===="
+echo "docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/openmm:7.7.0_49 run-benchmarks"
+docker run --rm -it --ipc=host --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined amdih/openmm:7.7.0_49 run-benchmarks
+echo "==== Run complete ===="
